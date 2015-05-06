76905dd59d87ab9b8d193b2b0649bac4879f986d
Mohammad Sharifi <mmdsharifi@yahoo.com>
2015-05-06 15:50:00 +0430

    New : Workflow Stup

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-var app = require('express')();
+//First : Create a Server for out app
+var express = require('express');
+// Define an express app variable ...
+var app = express();
+// and It can require a Server
 var http = require('http').Server(app);
- 
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000
+
+//say directories of assets
+app.use(express.static('./'));
+
+// Define a route handler
 app.get('/', function(req, res) {
-   res.send('<h1>Hello world</h1>');
+   res.sendFile(__dirname + '/index.html');
 });
  
-http.listen(3000, function() {
-    console.log('listening on *:3000');
+io.on('connection', function(socket){
+	console.log('a user connected');
+});
+
+// Create a HTTP server that listen on port ***
+
+http.listen(port, function () {
+	console.log('Server listening at port %d', port);
 });
 
